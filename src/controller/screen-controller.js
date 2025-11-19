eed50e275ee30ac9676cf2eee1b716f46e0aab73
IgorPVidal <55295666+IgorPVidal@users.noreply.github.com>
2025-11-19 05:36:06 +0000

    Modifica screen-controller.js para desenhar arestas do cubo no canvas

--- src/controller/screen-controller.js
+++ src/controller/screen-controller.js
@@ -1,8 +1,13 @@
+import { ScreenModel } from "../model/screen-model.js";
 import { ScreenView } from "../view/screen-view.js"
+import { CubeController } from "./cube-controller.js";
 
 export class ScreenController {
     //Private
+    #screenModel;
     #screenView;
+    #cubeController;
+    #playing;
     #show(element) {
         try {
             this.#screenView.show(element.draw())
@@ -10,13 +15,27 @@ export class ScreenController {
             console.log(`Error caught! [${error}]`)
         }
     }
+    #updateScreen() {
+        let edges = this.#screenModel.setEdges(this.#cubeController.getEdges())
+        this.clear()
+        this.#screenView.drawEdges(edges, this.#cubeController.getColor())
+    }
 
     //Public
-    constructor() {
-        this.#screenView = new ScreenView()
+    constructor(width = 320, height = 288) {
+        this.#screenModel = new ScreenModel(width, height)
+        this.#screenView = new ScreenView(width, height)
+        this.#cubeController = new CubeController()
+        this.#playing = true
     }
     async play() {
         this.#show(this)
+        console.log("start")
+        while (this.#playing) {
+            this.#updateScreen()
+            this.#playing = false
+        }
+        console.log("stop")
     }
     clear() {
         this.#screenView.clear()
